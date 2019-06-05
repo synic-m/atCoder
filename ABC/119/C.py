bee74dbdb1cf3a99ed42920ee83d8683d55e3c5d
synic <beatafly.eff3t@icloud.com>
2019-06-05 19:15:45 +0900

    ABC119 C -> AC   DFS

--- ABC/119/C.py
+++ ABC/119/C.py
@@ -3,6 +3,21 @@ N,A,B,C = map(int,input().split())
 l =[]
 for _ in range(N):
     l.append(int(input()))
+INF = 10**9
 
-print(N,A,B,C)
-print(l)
+def dfs(cur,a,b,c):
+    if cur == N:
+        if min(a,b,c) > 0:
+            ans = abs(a-A) + abs(b-B) + abs(c-C) -30 
+            #接合は各一回ずつ余分
+        else:
+            ans = INF
+        return ans
+    ret0 = dfs(cur+1,a+l[cur],b,       c)        + 10
+    ret1 = dfs(cur+1,a,       b+l[cur],c)        + 10
+    ret2 = dfs(cur+1,a,       b,       c+l[cur]) + 10
+    ret3 = dfs(cur+1,a,       b,       c)        + 10
+    #a,b,c,どこにも入れないの4通り
+    #分けて接合 -> 10
+    return min(ret0,ret1,ret2,ret3)
+print(dfs(0,0,0,0))
